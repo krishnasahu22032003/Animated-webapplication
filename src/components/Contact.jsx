11e4d1b97c06af1bdb3ebbe61a2dd237d38bc0de
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-06-18 23:25:39 +0530

    Fix incorrect button import path

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Button from './button';
+import Button from './Button';
 const ImageClipBox = ({ src, clipClass }) => (
   <div className={clipClass}>
     <img src={src} />
